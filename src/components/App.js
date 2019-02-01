7cff479162ce053883a303233a769b9ad460cf8a
Andrew <arhorn@smcm.edu>
2019-02-01 21:37:17 +0700

    added react-burger-menu and menu headings

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,16 @@
-import React from "react";
-import Sidebar from "react-sidebar";
- 
-const mql = window.matchMedia(`(min-width: 800px)`);
- 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarDocked: mql.matches,
-      sidebarOpen: false
-    };
- 
-    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
-    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-  }
- 
-  componentWillMount() {
-    mql.addListener(this.mediaQueryChanged);
-  }
- 
-  componentWillUnmount() {
-    this.state.mql.removeListener(this.mediaQueryChanged);
-  }
- 
-  onSetSidebarOpen(open) {
-    this.setState({ sidebarOpen: open });
-  }
- 
-  mediaQueryChanged() {
-    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
-  }
- 
-  render() {
-    return (
-      <Sidebar
-        sidebar={<b>Sidebar content</b>}
-        open={this.state.sidebarOpen}
-        docked={this.state.sidebarDocked}
-        onSetOpen={this.onSetSidebarOpen}
-      >
-        <b>Main content</b>
-      </Sidebar>
-    );
-  }
+import React from 'react';
+import SideBar from './Sidebar';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div id="App">
+      <SideBar />
+      <div id="page-wrap">
+        <h1>AppDividend</h1>
+        <h2>Check out our tutorials the side menubar</h2>
+      </div>
+    </div>
+  );
 }
- 
-export default App;
